5e696cb9ec074a5357aeb3fe6051b7f836ef8377
Zzzkvidi4 <zzzkvidi4@gmail.com>
2021-01-10 16:47:34 +0300

    Added manual connection configuration for .Net app

--- motivator/motivator/Models/TargeterContext.cs
+++ motivator/motivator/Models/TargeterContext.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 
@@ -28,7 +28,7 @@ namespace motivator.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=targeter;Username=postgres;Password=postgres");
+                optionsBuilder.UseNpgsql("Server=/cloudsql/targeter:us-central1:targeter-db;Uid=postgres;Pwd=postgres;Database=targeter");
             }
         }
         
