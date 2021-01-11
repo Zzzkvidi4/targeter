4924bf4363c085887343953453f7b26140fd7b5f
Zzzkvidi4 <zzzkvidi4@gmail.com>
2021-01-11 15:08:10 +0300

    enabled cloud configs

--- motivator/motivator/Models/TargeterContext.cs
+++ motivator/motivator/Models/TargeterContext.cs
@@ -28,9 +28,9 @@ namespace motivator.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseNpgsql("Host=localhost;Uid=postgres;Pwd=postgres;Database=targeter");
+                //optionsBuilder.UseNpgsql("Host=localhost;Uid=postgres;Pwd=postgres;Database=targeter");
                 //production db configuration
-                //optionsBuilder.UseNpgsql("Host=104.197.25.5;Uid=postgres;Pwd=postgres;Database=targeter");
+                optionsBuilder.UseNpgsql("Host=104.197.25.5;Uid=postgres;Pwd=postgres;Database=targeter");
 
             }
         }
